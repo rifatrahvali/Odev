3d97668af5e1f52413a01c7120596b7d2f5e09b6
RIFAT RAHVALI <rifatrahvali@outlook.com>
2024-03-09 00:47:36 +0300

    Malzemeleri - Ürünleri Sağ taraftaki card body içerisinde listeleyelim. eğer urunler listem boş veya daha önce eklenmediyse çalışacaktır. Listede ürün olmadığına dair bilgi verelim.  malzeme - ürün listesinin içerisinde elemanlarımız var ise foreach ile eleman  sayısı kadar li oluşturalım

--- assets/js/urun-ekleme.js
+++ assets/js/urun-ekleme.js
@@ -14,6 +14,43 @@ document.addEventListener("DOMContentLoaded",function(ee){ //Sayfa yüklendiğin
         urunler = [];
     }
 
+    // Malzemeleri - Ürünleri Sağ taraftaki card body içerisinde listeleyelim.
+    // eğer urunler listem boş veya daha önce eklenmediyse çalışacaktır.
+    if (urunler == null || Array.isArray(urunler) && urunler.length<1) {
+        // Listede ürün olmadığına dair bilgi verelim.
+
+        // DOM ile li element oluşturup müdahale edelim.
+        let liElement = document.createElement("li");
+        liElement.className = "list-group-item bg-warning text-white"
+        liElement.textContent = "Henüz listede bir malzeme - ürün yok."
+
+        // malzeme ekleme sayfasındaki sağ taraftaki ul li elemanları
+        let urunListesi = document.getElementById("urunListesi");
+        urunListesi.innerHTML = "";
+        urunListesi.appendChild(liElement); // ul içerisine varsa li'leri ekleyelim
+    }else{
+        // malzeme - ürün listesinin içerisinde elemanlarımız var ise
+        urunListesi.innerHTML = "";
+        // foreach ile elaman sayısı kadar li oluşturacak
+        urunler.forEach(function (urun,index,array) {
+
+            // li elementini oluşturduk
+            let liElement = document.createElement("li");
+            liElement.className = "list-group-item";
+            liElement.textContent = urun;
+
+            // i elementini oluşturduk
+            let iElement = document.createElement("i");
+            iElement.className = "bi bi-trash delete-product float-end";
+            iElement.id = index;
+            
+            urunListesi.appendChild(liElement);
+            liElement.appendChild(iElement);
+        });
+    }
+
+
+
     //btnUrunEkle Butonu İşlemleri - malzeme ekleme için.
     btnUrunEkle.addEventListener("click",function (e) {
         let urunAdi = document.getElementById("urunAdi").value;
