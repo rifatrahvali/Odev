0b48a55f541f7128fd708edfdce991232b3f9e62
RIFAT RAHVALI <rifatrahvali@outlook.com>
2024-03-09 00:05:44 +0300

    btnUrunEkle

--- assets/js/urun-ekleme.js
+++ assets/js/urun-ekleme.js
@@ -2,6 +2,23 @@
 // doküman(sayfa) içerisinde (elementler ile) işlem yapacağız. 
 
 // içerideki tüm elementlerin dinlenmesi - sayfanın yüklenmesini dinler
-document.addEventListener("DOMContentLoaded",function(event){
+document.addEventListener("DOMContentLoaded",function(sayfaYuklendi){
+    // Sayfa yüklendiğini belirtir.
     console.log("sayfa yüklendi");
+
+    // Malzeme - Ürün Ekle butonunu seçtik. Selector
+    let btnUrunEkle = document.getElementById("btnUrunEkle");
+    let urunler = [];
+
+    
+    btnUrunEkle.addEventListener("click",function (btnUrunEkleTiklandi) {
+        // alert("id : btnUrunEkle - tıklandı")
+
+        // urunAdi : text inputu seçtik
+        let urunAdi = document.getElementById("urunAdi").value;
+        console.log(urunAdi);
+
+        urunler.push[urunAdi];
+    });
 })
+
